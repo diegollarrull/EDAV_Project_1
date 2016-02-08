99bcdf9d11fc313398a397638be905c7034962c6
xz2476 <xz2476@columbia.edu>
2016-02-08 04:45:34 -0500

    R code to generate the chart plot(in case we change the data)

---
+++ R code to generate chart3 plot.R
@@ -0,0 +1,28 @@
+# R code to make the chart3 graph:
+install.packages("devtools")
+require(devtools)
+install_github('rCharts', 'ramnathv')(edited)
+require(rCharts)
+
+#generate data set
+names(survey)
+skills <- summary(survey[,13])
+for (i in c(17,15,14,16)){
+  x <- summary(survey[,i])
+  skills <- c(skills, x)
+}
+skills <- as.data.frame(skills)
+colnames(skills) <- "count"
+
+cols<-factor(c("R, graphic basics","Github","R, advanced ","Reproducible documentation","Matlab"))
+skills$skill <- rep(cols,each = 4)
+level <- factor(c("A little","Confident","Expert","None"))
+skills$level <- rep(level,5) 
+
+#rearrange orders:
+skills$level <- factor(skills$level,levels =c("Expert","Confident","A little","None"))
+skills <- with(skills,skills[order(level),])
+
+#generate D3 plot
+n <- nPlot(count ~ skill, group = "level", data = skills, type = "multiBarChart")
+n$print("chart3")
